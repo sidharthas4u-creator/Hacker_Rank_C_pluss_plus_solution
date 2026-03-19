7541272680633d84580e9849c3bca1042bae06b1
sidharth <sidharthas4u@gmail.com>
2026-03-19 20:15:52 +0530

    Create Basic_Data_Types.c
    hacker solution

---
+++ Basic_Data_Types.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c<<"\n";
+    cout<<d<<"\n";
+    cout<<e;
+    return 0;
+}
