a7c93a02161eaed44d182718b54316e8c367d9d9
sidharth <sidharthas4u@gmail.com>
2026-03-19 20:44:34 +0530

    Create Arrays_Introduction.c
    hacker rank solution

---
+++ Arrays_Introduction.c
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+        int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    /* Write the logic to reverse the array. */
+    for(int i=(num-1);i>=0;i--)
+    {
+           printf("%d ", arr[i]);
+    }
+    return 0;
+}
