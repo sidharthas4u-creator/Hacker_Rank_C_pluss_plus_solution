56ea6dfb545f41ffdf98752c9a89bbe1f3505053
sidharth <sidharthas4u@gmail.com>
2026-03-19 20:31:05 +0530

    Create For_Loop.c
    hacker rank solution

---
+++ For_Loop.c
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+      int a, b;
+    scanf("%d\n%d", &a, &b);
+        for(int i=a;i<=b;i++)
+    {
+        int n=i;
+         if(0<n && n<=9 && n<10)
+    {
+        if(1==n)
+        {
+            printf("one");
+        }
+        if(2==n)
+        {
+            printf("two");
+        }
+        if(3==n)
+        {
+            printf("three");
+            
+        }
+        if(4==n)
+        {
+            printf("four");
+        }
+        if(5==n)
+        {
+            printf("five");
+        }
+        if(6==n)
+        {
+            printf("six");
+        }
+        if(7==n)
+        {
+            printf("seven");
+        }
+        if(8==n)
+        {
+            printf("eight");
+        }
+        if(9==n)
+        {
+            printf("nine");
+        }
+        
+    }
+    else
+    {
+       if (i%2==0)
+       {
+        printf("even");
+       }
+       else
+       {
+        printf("odd");
+       }
+    }
+      printf("\n");
+    }
+    return 0;
+}
